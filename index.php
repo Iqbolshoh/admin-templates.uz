4cac8d5bc75fbb5c175842258998b6086a4de6b9
Iqbolshoh <148738947+Iqbolshoh@users.noreply.github.com>
2024-08-08 10:49:39 +0500

    commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description"
         content="Welcome to the website of Iqbolshoh, a Full-Stack Developer from Samarkand with 3+ years of experience. Specializing in web development. Contact me at +998997799333.">
     <meta name="keywords"
-        content="ilhomjonov, iqbolshoh, iqbolshoh ilhomjonov, ikromjonovich, iqbolshoh_777, programmer_777, software developer, full-stack developer, javascript developer, web developer, programmer, samarkand developer, uzbekistan developer, programming, web development, github projects, software engineer, dasturchi, programmist, dasturiy taminot, web dasturchi, iqbolshoh.uz, templates.uz">
+        content="ilhomjonov, iqbolshoh, iqbolshoh ilhomjonov, ikromjonovich, iqbolshoh_777, programmer_777, software developer, full-stack developer, javascript developer, web developer, programmer, samarkand developer, uzbekistan developer, programming, web development, github projects, software engineer, dasturchi, programmist, dasturiy taminot, web dasturchi, iqbolshoh.uz, templates.uz, +998997799333, 997799333">
 
     <!-- Open Graph meta tags -->
     <meta property="og:title" content="Iqbolshoh Ilhomjonov - Full-Stack Developer">
@@ -27,13 +27,14 @@
     <meta name="twitter:image" content="https://templates.uz/images/iqbolshoh-1.jpg">
     <meta name="twitter:site" content="@iqbolshoh_777">
 
+
     <meta name="author" content="Iqbolshoh Ilhomjonov">
     <meta name="robots" content="index, follow">
     <meta name="application-name" content="Iqbolshoh's Website">
     <meta name="theme-color" content="#ffffff">
 
     <title>Iqbolshoh Ilhomjonov</title>
-    <link rel="icon" href="/images/favicon.ico">
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 
